c77a7aa23d65acc0df9a099a3dd45d0960d98a8f
SUNGYO <108318375+SunGYOkIM@users.noreply.github.com>
2022-09-19 10:53:37 +0900

    3 commit(팀원 commit)(Myboot1Application)

--- myboot1_github20220919/src/main/java/com/example/demo/Myboot1Application.java
+++ myboot1_github20220919/src/main/java/com/example/demo/Myboot1Application.java
@@ -26,6 +26,7 @@ public class Myboot1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Myboot1Application.class, args);
 		System.out.println("프로젝트 팀장이 추가하는 라인");
+		System.out.println("프로젝트 팀원이 추가하는 라인");
 	}
 
 }
